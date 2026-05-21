49084a45f305f1fe6d441bb5e2b8d6a81edbf204
Xx96Lussy_Pover69xX <accordigioele@gmail.com>
2026-05-21 13:12:26 +0100

    Delete backend directory

--- backend/src/main/java/com/proyecto/perfumeHud/repositories/PerfumeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.perfumeHud.repositories;
-
-import com.proyecto.perfumeHud.model.Perfume;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@CrossOrigin(origins = "http://localhost:4200") 
-public interface PerfumeRepository extends JpaRepository<Perfume, Long> {
-}
